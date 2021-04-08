a921542ba1189540f52b9bbecb58a2173768fe20
janbarend66 <72501763+janbarend66@users.noreply.github.com>
2021-04-08 13:29:42 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -184,4 +184,34 @@ function wc_rest_user_endpoint_handler($request = null) {
 }
 
 add_theme_support( 'post-thumbnails' );
+
+add_action('rest_api_init', 'register_rest_images' );
+function register_rest_images(){
+    register_rest_field( array('app'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
+
+add_action( 'fire_this_function', 'my_custom_callback_function', 20, 4);
+function my_custom_callback_function( $return_args, $post_id, $thumbnail_size, $post_taxonomies ){
+    $data = array(
+        'ID' => $post_id,
+        'post_title' => 'gert',
+    );
+
+    wp_update_post( $data );
+    //run your custom logic in here
+}
 ?>
